f17f415a835b219dac64860102a96740ab42df73
mfahdaz <fahadazeemi@gmail.com>
2022-05-03 12:36:38 +0000

    added client to Proxy Server Maanger

--- common/utils/proxy_manager_server_utils.py
+++ common/utils/proxy_manager_server_utils.py
@@ -4,34 +4,7 @@
 #  license agreements; and to You under the Apache License, Version 2.0. "
 # ------------------------------------------------------------------------------
 
-from multiprocessing.managers import BaseManager
-from EBRAINS_RichEndpoint.Application_Companion.common_enums import Response
-
-
 # NOTE: later change these hardcoded values to be set during runtime
 IP = '127.0.0.1'
 PORT = 50000
 KEY = b'secret'
-
-def connect(ip, port, key):
-    class Manager(BaseManager): pass
-    Manager.register('ServiceRegistryManager')
-    proxy_manager_client = Manager(address=(ip, port), authkey=key)
-    try:
-        proxy_manager_client.connect()
-    except ConnectionRefusedError as e:
-        # Case a: connection is refused. print the exception and
-        # return Error response to terminate
-        print(e)
-        return Response.ERROR
-    
-    # Case b: connection is established. Get proxy to registry manager
-    return proxy_manager_client
-
-def get_registry_proxy(proxy_manager_client, log_settings, configurations_manager):
-    return proxy_manager_client.ServiceRegistryManager(log_settings, configurations_manager)
-
-def terminate_with_error(exception):
-    print(f"EXCEPTION: {exception}\n\n")
-    # raise RuntimeError exception to terminate
-    raise RuntimeError
