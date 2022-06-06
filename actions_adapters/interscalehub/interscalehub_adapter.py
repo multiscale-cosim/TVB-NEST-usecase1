898e77162dbf0b724f69d2cb2f8483fb7e180c4d
user <user@rocky85.localdomain>
2022-06-06 05:03:26 +0200

    Sci-Params for Inter-Scale-Hub

--- actions_adapters/interscalehub/interscalehub_adapter.py
+++ actions_adapters/interscalehub/interscalehub_adapter.py
@@ -19,18 +19,19 @@ import base64
 
 from common.utils.security_utils import check_integrity
 from actions_adapters.setup_result_directories import SetupResultDirectories
-from EBRAINS_InterscaleHUB.Interscale_hub.manager_nest_to_tvb import  NestToTvbManager
+from EBRAINS_InterscaleHUB.Interscale_hub.manager_nest_to_tvb import NestToTvbManager
 from EBRAINS_InterscaleHUB.Interscale_hub.manager_tvb_to_nest import TvbToNestManager
 from EBRAINS_InterscaleHUB.Interscale_hub.interscalehub_enums import DATA_EXCHANGE_DIRECTION 
 from EBRAINS_ConfigManager.global_configurations_manager.xml_parsers.default_directories_enum import DefaultDirectories
 from EBRAINS_ConfigManager.global_configurations_manager.xml_parsers.configurations_manager import ConfigurationsManager
 
 
-def run_wrapper(direction, configurations_manager, log_settings):
-    '''
+def run_wrapper(direction, configurations_manager, log_settings,
+                sci_params_xml_path_filename=''):
+    """
     starts the inter-scale hub to exchange data in direction provided as the
     parameter.
-    '''
+    """
     # direction
     # 1 --> nest to Tvb
     # 2 --> tvb to nest
@@ -42,13 +43,14 @@ def run_wrapper(direction, configurations_manager, log_settings):
                 "path": path,
                 "simulation_time": 1000.0,
                 "level_log": 1,
-                "resolution": 0.1,
-                "nb_neurons": [100],
+                # "resolution": 0.1, -> on interscale_hub.xml as dt
+                # POD: is nb_neurons a functional parameter?
+                # "nb_neurons": [100], -> on interscale_hub.xml, POD: is a Python's List required?
                 # parameter for the synchronization between simulators
-                "time_synchronization": 1.2,
-                "id_nest_region": [0],
+                # "time_synchronization": 1.2, -> on interscale_hub.xml
+                "id_nest_region": [0],  # TO BE DONE: supporting Python's list on XML sci-params
                 # parameter for the transformation of data between scale
-                "nb_brain_synapses": 1,
+                # "nb_brain_synapses": 1, -> on interscale_hub.xml
                 'id_first_neurons': [1],
                 "save_spikes": True,
                 "save_rate": True,
@@ -56,22 +58,28 @@ def run_wrapper(direction, configurations_manager, log_settings):
                 "id_first_spike_detector": 229
         }
 
-    direction = int(direction) # NOTE: will be changed
+    direction = int(direction)  # NOTE: will be changed
     
     # Case a: Nest to TVB inter-scale hub
     if direction == DATA_EXCHANGE_DIRECTION.NEST_TO_TVB:
         # create directories to store parameter.json file, 
         # port information, and logs
         SetupResultDirectories(path)  # NOTE: will be changed
-        hub = NestToTvbManager(parameters, configurations_manager, log_settings)
+        hub = NestToTvbManager(parameters,
+                               configurations_manager,
+                               log_settings,
+                               sci_params_xml_path_filename=sci_params_xml_path_filename)
     
     # Case b: TVB to NEST inter-scale hub
     elif direction == DATA_EXCHANGE_DIRECTION.TVB_TO_NEST:
         # let the NEST_TO_TVB inter-scale hub to set up the directories and
         # parameters
         time.sleep(1)
-        hub = TvbToNestManager(parameters, direction, configurations_manager,
-                               log_settings)
+        hub = TvbToNestManager(parameters,
+                               direction,  # NOTE: is it needed?
+                               configurations_manager,
+                               log_settings,
+                               sci_params_xml_path_filename=sci_params_xml_path_filename)
 
     # 1) init steering command
     # includes param setup, buffer creation
@@ -97,4 +105,7 @@ if __name__ == '__main__':
     check_integrity(configurations_manager, ConfigurationsManager)
     check_integrity(log_settings, dict)
     # everything is fine, run InterscaleHub
-    sys.exit(run_wrapper(direction, configurations_manager, log_settings))
+    sys.exit(run_wrapper(direction,
+                         configurations_manager,
+                         log_settings,
+                         sci_params_xml_path_filename=sys.argv[4]))
