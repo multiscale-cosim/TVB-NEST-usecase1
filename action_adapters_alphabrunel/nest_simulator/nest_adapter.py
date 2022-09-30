eff242bda854b170dc0a0ff2a58646459e908806
mfahdaz <fahadazeemi@gmail.com>
2022-09-30 15:47:05 +0000

    Sprint 9: setting up data channels using Registry Service

--- action_adapters_alphabrunel/nest_simulator/nest_adapter.py
+++ action_adapters_alphabrunel/nest_simulator/nest_adapter.py
@@ -16,7 +16,6 @@ import sys
 import pickle
 import base64
 
-from action_adapters_alphabrunel.nest_simulator.utils_function import wait_transformation_modules
 from action_adapters_alphabrunel.nest_simulator.utils_function import get_data
 from common.utils.security_utils import check_integrity
 from action_adapters_alphabrunel.parameters import Parameters
@@ -35,7 +34,7 @@ import matplotlib.pyplot as plt
 
 class NESTAdapter:
     def __init__(self, p_configurations_manager, p_log_settings,
-                 p_interscalehub_address,
+                 p_interscalehub_addresses,
                  sci_params_xml_path_filename=None):
         self._log_settings = p_log_settings
         self._configurations_manager = p_configurations_manager
@@ -48,25 +47,46 @@ class NESTAdapter:
 
         # Loading scientific parameters into an object
         self.__sci_params = Xml2ClassParser(sci_params_xml_path_filename, self.__logger)
-
         self.__parameters = Parameters(self.__path_to_parameters_file)
 
-        # The name of the MPI port to send data to needs to be in string
-        # format and named according to the following pattern:
-        # endpoint_address:<port address>
-        for interscalehub in p_interscalehub_address:
+        # The MPI port_name needs to be in string format and must be sent to
+        # nest-simulator in the following pattern:
+
+        # "endpoint_address":<port name>
+
+        # Initialize port_names in the format as per nest-simulator
+        self.__init_port_names(p_interscalehub_addresses)
+        self.__logger.info("initialized")
+
+    def __init_port_names(self, interscalehub_addresses):
+        '''
+        helper function to prepare the port_names in the following format:
+
+        "endpoint_address":<port name>
+        '''
+        for interscalehub in interscalehub_addresses:
             self.__logger.debug(f"running interscalehub: {interscalehub}")
             # NEST_TO_TVB RECEIVER endpoint
-            if interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.NEST_TO_TVB.name:
-                self.__interscalehub_nest_to_tvb_address = "endpoint_address:"+interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
-                self.__logger.info(f"__DEBUG__ interscalehub_nest_to_tvb_address: {self.__interscalehub_nest_to_tvb_address}")
+            if interscalehub.get(
+                    INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) ==\
+                    DATA_EXCHANGE_DIRECTION.NEST_TO_TVB.name:
+                # get mpi port name
+                self.__interscalehub_nest_to_tvb_address =\
+                    "endpoint_address:"+interscalehub.get(
+                        INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
+                self.__logger.debug("Interscalehub_nest_to_tvb_address: "
+                                    f"{self.__interscalehub_nest_to_tvb_address}")
 
             # TVB_TO_NEST SENDER endpoint
-            elif interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.TVB_TO_NEST.name:
-                self.__interscalehub_tvb_to_nest_address = "endpoint_address:"+interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
-                self.__logger.info(f"__DEBUG__ interscalehub_tvb_to_nest_address: {self.__interscalehub_tvb_to_nest_address}")
-
-        self.__logger.info("initialized")
+            elif interscalehub.get(
+                    INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) ==\
+                    DATA_EXCHANGE_DIRECTION.TVB_TO_NEST.name:
+                # get mpi port name
+                self.__interscalehub_tvb_to_nest_address =\
+                    "endpoint_address:"+interscalehub.get(
+                        INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
+                self.__logger.debug("Interscalehub_tvb_to_nest_address: "
+                                    f"{self.__interscalehub_tvb_to_nest_address}")
 
     def __configure_nest(self, simulator):
         """
@@ -171,9 +191,6 @@ class NESTAdapter:
         # input_to_simulator = simulator.Create("spike_generator", self.__parameters.nb_neurons,
         #                                       params={'stimulus_source': 'mpi',
         #                                               'label': '/../transformation/spike_generator'})
-        # input_to_simulator = simulator.Create(model=self.__sci_params.input_to_simulator['model'],
-        #                                       n=self.__sci_params.nb_neurons,
-        #                                       params=self.__sci_params.input_to_simulator['params'])
         input_to_simulator = simulator.Create(model=self.__sci_params.input_to_simulator['model'],
                                               n=self.__sci_params.nb_neurons,
                                               params={'stimulus_source': 'mpi',
@@ -181,11 +198,10 @@ class NESTAdapter:
         # output_from_simulator = simulator.Create("spike_recorder",
         #                                          params={"record_to": "mpi",
         #                                                  'label': '/../transformation/spike_detector'})
-        # output_from_simulator = simulator.Create(model=self.__sci_params.output_from_simulator['model'],
-        #                                          params=self.__sci_params.output_from_simulator['params'])
         output_from_simulator = simulator.Create("spike_recorder",
                                                  params={"record_to": "mpi",
                                                          'label': self.__interscalehub_nest_to_tvb_address})
+        
         # simulator.Connect(input_to_simulator, nodes_ex, {'rule': 'one_to_one'},
         #                   {"weight": 20.68015524367846, "delay": 0.1})
         simulator.Connect(pre=input_to_simulator,
@@ -205,21 +221,16 @@ class NESTAdapter:
         self.__logger.debug("executing INIT command")
         nest.ResetKernel()
         nest.SetKernelStatus(
-            {"data_path": self.__parameters.path + '/nest/', "overwrite_files": True, "print_time": True,
+            {"data_path": self.__parameters.path + '/nest/',
+             "overwrite_files": True, "print_time": True,
              "resolution": self.__parameters.resolution})
 
         self.__logger.info("configure the network")
         espikes, input_to_simulator, output_from_simulator = \
             self.__configure_nest(nest)
 
-        self.__logger.info("establishing the connections")
-        # wait_transformation_modules(
-        #     nest,
-        #     self.__parameters.path,
-        #     input_to_simulator,
-        #     output_from_simulator,
-        #     self.__logger)
-        self.__logger.info("preparing the simulator")
+        self.__logger.info("preparing the simulator, and "
+                           "establishing the connections")
         nest.Prepare()
         self.__logger.info("connections are made")
         self.__logger.debug("INIT command is executed")
@@ -230,10 +241,11 @@ class NESTAdapter:
         count = 0.0
         self.__logger.debug('starting simulation')
         while count * self.__parameters.time_synch < self.__parameters.simulation_time:
+            self.__logger.info(f"simulation run counter: {count+1}")
             nest.Run(self.__parameters.time_synch)
             count += 1
 
-        self.__logger.debug('nest simulation is finished')
+        self.__logger.info('nest simulation is finished')
         self.__logger.info("cleaning up NEST")
         nest.Cleanup()
         # self.execute_end_command()
@@ -258,17 +270,13 @@ class NESTAdapter:
 
 
 if __name__ == "__main__":
-
+    # TODO better handling of arguments parsing
     # unpickle configurations_manager object
     configurations_manager = pickle.loads(base64.b64decode(sys.argv[2]))
-
     # unpickle log_settings
-    log_settings = pickle.loads(base64.b64decode(sys.argv[3]))    
-
+    log_settings = pickle.loads(base64.b64decode(sys.argv[3]))
     # get science parameters XML file path
     p_sci_params_xml_path_filename = sys.argv[4]
-
-
     # get interscalehub connection details
     p_interscalehub_address = pickle.loads(base64.b64decode(sys.argv[5]))
 
@@ -278,7 +286,7 @@ if __name__ == "__main__":
     check_integrity(log_settings, dict)
     check_integrity(p_interscalehub_address, list)
 
-    # everything is fine, run simulation
+    # everything is fine, configure simulator
     nest_adapter = NESTAdapter(configurations_manager, log_settings,
                                p_interscalehub_address,
                                sci_params_xml_path_filename=p_sci_params_xml_path_filename)
@@ -287,16 +295,14 @@ if __name__ == "__main__":
 
     # send local minimum step size to Application Manager as a response to INIT
     # NOTE Application Manager expects a string in the following format:
-    # {'PID': <int>, 'LOCAL_MINIMUM_STEP_SIZE': <float>}
+    # {'PID': <pid>, 'LOCAL_MINIMUM_STEP_SIZE': <step size>}
     pid_and_local_minimum_step_size = \
         {SIMULATOR.PID.name: os.getpid(),
          SIMULATOR.LOCAL_MINIMUM_STEP_SIZE.name: local_minimum_step_size}
-
     # Application Manager will read the stdout stream via PIPE
-    # NOTE the communication with Application Manager via PIPES will be
-    # changed to some other mechanism
     print(f'{pid_and_local_minimum_step_size}')
 
+    # fetch next command from Application Manager
     user_action_command = input()
     # execute if steering command is START
     if SteeringCommands[user_action_command] == SteeringCommands.START:
--- action_adapters_alphabrunel/tvb_simulator/tvb_adapter.py
+++ action_adapters_alphabrunel/tvb_simulator/tvb_adapter.py
@@ -40,7 +40,7 @@ numpy.random.seed(125)
 class TVBAdapter:
 
     def __init__(self, p_configurations_manager, p_log_settings,
-                 p_interscalehub_address, p_sci_params_xml_path_filename=None):
+                 p_interscalehub_addresses, p_sci_params_xml_path_filename=None):
         self._log_settings = p_log_settings
         self._configurations_manager = p_configurations_manager
         self.__logger = self._configurations_manager.load_log_configurations(
@@ -50,27 +50,37 @@ class TVBAdapter:
         self.__path_to_parameters_file = self._configurations_manager.get_directory(
             directory=DefaultDirectories.SIMULATION_RESULTS)
 
-        # Loading scientific parameters into an object
+        # Load scientific parameters into an object
         self.__sci_params = Xml2ClassParser(p_sci_params_xml_path_filename, self.__logger)
 
         self.__parameters = Parameters(self.__path_to_parameters_file)
         self.__simulator_tvb = None
         self.__tvb_mpi_wrapper = None
-        self.__logger.debug(f"interscalehubs: {p_interscalehub_address}")
-        self.__interscalehub_address = p_interscalehub_address
-
-        for interscalehub in p_interscalehub_address:
-            self.__logger.debug(f"running interscalehub: {interscalehub}")
-            if interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.NEST_TO_TVB.name:
-                self.__interscalehub_nest_to_tvb_address = interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
-                self.__logger.info(f"__DEBUG__ interscalehub_nest_to_tvb_address: {self.__interscalehub_nest_to_tvb_address}")
-            elif interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.TVB_TO_NEST.name:
-                self.__interscalehub_tvb_to_nest_address = interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
-                self.__logger.info(f"__DEBUG__ interscalehub_tvb_to_nest_address: {self.__interscalehub_tvb_to_nest_address}")
-
+        # initialize port_names of the Interscalehubs
+        self.__init_port_names(p_interscalehub_addresses)
         self.__logger.info("initialized")
 
-    #
+    def __init_port_names(self, interscalehub_addresses):
+        '''
+        helper function to initialize the port_names
+        '''
+        self.__logger.debug("Interscalehubs endpoints: "
+                            f" {interscalehub_addresses}")
+
+        for interscalehub in interscalehub_addresses:
+            self.__logger.debug(f"running interscalehub: {interscalehub}")
+            # NEST_TO_TVB RECEIVER endpoint
+            if interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.NEST_TO_TVB.name:
+                self.__interscalehub_nest_to_tvb_address =\
+                    interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
+                self.__logger.debug(f"Interscalehub_nest_to_tvb_address: {self.__interscalehub_nest_to_tvb_address}")
+
+            # TVB_TO_NEST SENDER endpoint
+            elif interscalehub.get(INTERSCALE_HUB.DATA_EXCHANGE_DIRECTION.name) == DATA_EXCHANGE_DIRECTION.TVB_TO_NEST.name:
+                self.__interscalehub_tvb_to_nest_address =\
+                    interscalehub.get(INTERSCALE_HUB.MPI_CONNECTION_INFO.name)
+                self.__logger.debug(f"Interscalehub_tvb_to_nest_address: {self.__interscalehub_tvb_to_nest_address}")
+
     # def __configure(self, time_synch=0.1, id_nest_region=None, dt=0.1):
     def __configure(self):
         """
@@ -120,11 +130,12 @@ class TVBAdapter:
         self.__simulator_tvb = self.__configure()
         self.__simulator_tvb.simulation_length = self.__parameters.simulation_time
         # set up MPI connections
-        self.__tvb_mpi_wrapper = TVBMpiWrapper(self._log_settings,
-                                               self._configurations_manager,
-                                               self.__simulator_tvb,
-                                               intercalehub_nest_to_tvb=self.__interscalehub_nest_to_tvb_address,
-                                               intercalehub_tvb_to_nest=self.__interscalehub_tvb_to_nest_address)
+        self.__tvb_mpi_wrapper = TVBMpiWrapper(
+            self._log_settings,
+            self._configurations_manager,
+            self.__simulator_tvb,
+            intercalehub_nest_to_tvb=self.__interscalehub_nest_to_tvb_address,
+            intercalehub_tvb_to_nest=self.__interscalehub_tvb_to_nest_address)
         self.__tvb_mpi_wrapper.init_mpi()
         self.__logger.debug("INIT command is executed")
         return self.__parameters.time_synch  # minimum step size for simulation 
@@ -145,17 +156,13 @@ class TVBAdapter:
 
 
 if __name__ == "__main__":
-    # TODO receive port info as an argument, i.e. argv[4]
-    # TODO pass port info to start command
+    # TODO better handling of arguments parsing
     # unpickle configurations_manager object
     configurations_manager = pickle.loads(base64.b64decode(sys.argv[2]))
     # unpickle log_settings
     log_settings = pickle.loads(base64.b64decode(sys.argv[3]))
-    
-
     # get science parameters XML file path
     p_sci_params_xml_path_filename = sys.argv[4]
-
     # get interscalehub connection details
     p_interscalehub_address = pickle.loads(base64.b64decode(sys.argv[5]))
 
@@ -165,24 +172,22 @@ if __name__ == "__main__":
     check_integrity(log_settings, dict)
     check_integrity(p_interscalehub_address, list)
 
-    # start simulation
+    # configure simulator
     tvb_adapter = TVBAdapter(configurations_manager,
                              log_settings,
                              p_interscalehub_address,
                              p_sci_params_xml_path_filename=p_sci_params_xml_path_filename)
-
     local_minimum_step_size = tvb_adapter.execute_init_command()
     # send local minimum step size to Application Manager as a response to INIT
     # NOTE Application Manager expects a string in the following format:
-    # {'PID': <int>, 'LOCAL_MINIMUM_STEP_SIZE': <float>}
+    # {'PID': <pid>, 'LOCAL_MINIMUM_STEP_SIZE': <step size>}
     pid_and_local_minimum_step_size = \
         {SIMULATOR.PID.name: os.getpid(),
          SIMULATOR.LOCAL_MINIMUM_STEP_SIZE.name: local_minimum_step_size}
-
     # Application Manager will read the stdout stream via PIPE
-    # NOTE the communication with Application Manager via PIPES will be
-    # changed to some other mechanism
     print(f'{pid_and_local_minimum_step_size}')
+
+    # fetch next command from Application Manager
     user_action_command = input()
     # execute if steering command is START
     if SteeringCommands[user_action_command] == SteeringCommands.START:
